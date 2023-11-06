e2747ed6d00c987b007b93d4a9768479d0376edb
PraveenVishvakarma <praveen15100@gmail.com>
2023-11-06 18:34:04 +0530

    create update listing api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,11 @@
 import  express  from "express";
-import { create, deleteListing } from "../controllers/listing.contoller.js";
+import { create, deleteListing, updateListing } from "../controllers/listing.contoller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const listingRouter=express.Router();
 
 listingRouter.post("/create", verifyToken, create);
 listingRouter.delete("/delete/:id", verifyToken, deleteListing);
+listingRouter.post("/update/:id", verifyToken, updateListing);
 
 export default listingRouter;
