b8d06376ae47321ea6818947871d45e65cfcce05
PraveenVishvakarma <praveen15100@gmail.com>
2023-11-08 19:10:20 +0530

    create search api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import  express  from "express";
-import { create, deleteListing, getListing, updateListing } from "../controllers/listing.contoller.js";
+import { create, deleteListing, getListing, getListings, updateListing } from "../controllers/listing.contoller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const listingRouter=express.Router();
@@ -8,5 +8,6 @@ listingRouter.post("/create", verifyToken, create);
 listingRouter.delete("/delete/:id", verifyToken, deleteListing);
 listingRouter.post("/update/:id", verifyToken, updateListing);
 listingRouter.get("/get/:id", getListing);
+listingRouter.get("/get", getListings);
 
 export default listingRouter;
--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -34,7 +34,13 @@ export default function Contact({listing}) {
     <>
     {landlord && <div className='flex flex-col gap-2'>
         <p>Contact <span className='font-semibold'>{landlord.username}</span> for <span className='font-semibold'>{listing.name.toLowerCase()}</span></p>
-        <textarea onChange={(e)=>setMessage(e.target.value)} placeholder='Enter your message here' name="message" value={message} className='rounded-lg w-full p-3 border' id="message" rows="2"></textarea>
+        <textarea
+         onChange={(e)=>setMessage(e.target.value)} 
+         placeholder='Enter your message here' name="message" 
+         value={message} className='rounded-lg w-full p-3 border' 
+         id="message" rows="2"
+         >
+         </textarea>
         <Link
           to={`mailto:${landlord.email}?subject=Regarding${listing.name}&body=${message}`}
         >
