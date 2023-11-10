d5b965de8449676358df7ced7c4e51c50e76c6ae
PraveenVishvakarma <praveen15100@gmail.com>
2023-11-10 21:37:24 +0530

    changes for production

--- api/index.js
+++ api/index.js
@@ -14,7 +14,9 @@ mongoose.connect(process.env.MONGO).then(()=>{
 })
 .catch((err)=>{
     console.log(err);
-})
+});
+
+const _dirname=path.resolve();
 
 const app=express();
 app.use(express.json());
@@ -23,6 +25,12 @@ app.use(cookieParser());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(_dirname,'/client/dist')));
+
+app.get('*',(req, res)=>{
+    res.sendFile(path.join(_dirname,'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next)=>{
     const statusCode=err.statusCode || 500;
