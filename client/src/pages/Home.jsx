cb412a4931a42acd75a4ba1f729c97faf27f5371
PraveenVishvakarma <125988954+PraveenVishvakarma@users.noreply.github.com>
2024-03-01 02:40:41 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -45,7 +45,7 @@ export default function Home() {
         place with ease
         </h1>
         <div className='text-gray-400 text-xs sm:text-sm'>
-          Rohit Estate is the best place to find your next perfect place to live.
+          Praveen Estate is the best place to find your next perfect place to live.
           <br />
           We have a wide range of properties for you to choose from.
         </div>
